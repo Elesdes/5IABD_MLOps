6899f60195077e38e8cdae17afc638907bab1462
ErwanDuprey <erwan.duprey@free.fr>
2024-05-01 22:17:25 +0200

    load_data.py patched. Return feature corrected

--- steps/loader_step/load_data.py
+++ steps/loader_step/load_data.py
@@ -4,15 +4,13 @@ from zenml import step
 
 
 @step()
-def load_data() -> tuple[
-    Annotated[pd.DataFrame, "clean_features"], Annotated[pd.DataFrame, "raw_features"]
-]:
+def load_data() -> Annotated[pd.DataFrame, "clean_features"]:
     """
     Dataset reader steps.
 
     This steps will read the data from csv files locally.
     :return:
-    The dataset pre-cleaned and the raw dataset
+    The dataset pre-cleaned
     """
     df = pd.read_csv("./data/emg_all_features_labeled.csv")
     return df
