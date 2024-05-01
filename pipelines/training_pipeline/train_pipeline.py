38bb272ceb77c11f15b2f9dd8cf303df53b3c3e8
ErwanDuprey <erwan.duprey@free.fr>
2024-05-01 17:19:26 +0200

    Alerter discord added

--- pipelines/training_pipeline/train_pipeline.py
+++ pipelines/training_pipeline/train_pipeline.py
@@ -1,6 +1,7 @@
 import zenml
 from zenml import pipeline
 # from zenml.config import DockerSettings
+from zenml.integrations.discord.steps.discord_alerter_ask_step import discord_alerter_ask_step
 from steps.loader_step.load_data import load_data
 from steps.training_step.data_preprocessing import data_preprocessing
 from steps.training_step.plot_data import plot_data
@@ -29,6 +30,7 @@ def train_pipeline():
     model_XGB, y_xgboost_predicted = train_xgboost(x_train, y_train, x_test)
     model_randomforest, y_randomforest_predicted = train_randomforest(x_train, y_train, x_test)
     train_custom_model(x_train, y_train, x, batch_size, epochs)
-    accuracy_xgb, accuracy_randomforest = accuracy_score_global(y_xgboost_predicted, y_randomforest_predicted, y_test)
+    accuracy_xgb, accuracy_randomforest, message = accuracy_score_global(y_xgboost_predicted, y_randomforest_predicted, y_test)
+    approved = discord_alerter_ask_step(message)
     # mlflow_register_model_step(model=model_XGB, name="model_XGB", metadata=accuracy_xgb)
     # mlflow_register_model_step(model=model_randomforest, name="model_randomforest", metadata=accuracy_randomforest)
--- steps/training_step/accuracy_score_global.py
+++ steps/training_step/accuracy_score_global.py
@@ -11,20 +11,20 @@ import numpy
 
 
 @step()
-def accuracy_score_global(y_xgb: numpy.ndarray, y_randomforest: numpy.ndarray, y_test: pd.DataFrame) -> tuple[float, float]:
+def accuracy_score_global(y_xgb: numpy.ndarray, y_randomforest: numpy.ndarray, y_test: pd.DataFrame) -> tuple[float, float, str]:
     """
     Accuracy score validation step
     :param y_xgb: The predicted output of the XGB model
     :param y_randomforest: The predicted output of the Random Forest model
     :param y_test: The testing dataframe output
     :return:
-    Double accuracies of XGB and RandomForest
+    Double accuracies of XGB and RandomForest and the string for discord
     """
-    accuracy_xgb = accuracy_score(y_test, [r for r in y_xgb])
-    accuracy_randomforest = accuracy_score(y_test, [q for q in y_randomforest])
-    print("Accuracy for XGB: %.2f%%" % (accuracy_xgb * 100.0))
-    print("Accuracy for RF: %.2f%%" % (accuracy_randomforest * 100.0))
-    return accuracy_xgb * 100.0, accuracy_randomforest * 100.0
+    accuracy_xgb = accuracy_score(y_test, [r for r in y_xgb]) * 100.0
+    accuracy_randomforest = accuracy_score(y_test, [q for q in y_randomforest])* 100.0
+    print("Accuracy for XGB: %.2f%%" % (accuracy_xgb))
+    print("Accuracy for RF: %.2f%%" % (accuracy_randomforest))
+    return accuracy_xgb, accuracy_randomforest, "Ended correctly with Accuracy for XGB: %.2f%% and Accuracy for RF: %.2f%%" % (accuracy_xgb * 100.0, accuracy_randomforest * 100.0)
     """
     plt.plot(range(epochs), history.history['loss'], label="Training Loss")
     plt.plot(range(epochs), history.history['val_loss'], label="Validation Loss")
