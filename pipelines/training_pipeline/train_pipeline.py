f7064b3a267afa677a589154b9700aea46a38f8e
Elesdes <elesdes404@gmail.com>
2024-05-01 18:27:48 +0200

    Refacto pre-commit with black #2

--- pipelines/training_pipeline/train_pipeline.py
+++ pipelines/training_pipeline/train_pipeline.py
@@ -2,7 +2,9 @@ import zenml
 from zenml import pipeline
 
 # from zenml.config import DockerSettings
-from zenml.integrations.discord.steps.discord_alerter_ask_step import discord_alerter_ask_step
+from zenml.integrations.discord.steps.discord_alerter_ask_step import (
+    discord_alerter_ask_step,
+)
 from steps.loader_step.load_data import load_data
 from steps.training_step.data_preprocessing import data_preprocessing
 from steps.training_step.plot_data import plot_data
@@ -35,7 +37,9 @@ def train_pipeline():
         x_train, y_train, x_test
     )
     train_custom_model(x_train, y_train, x, batch_size, epochs)
-    accuracy_xgb, accuracy_randomforest, message = accuracy_score_global(y_xgboost_predicted, y_randomforest_predicted, y_test)
+    accuracy_xgb, accuracy_randomforest, message = accuracy_score_global(
+        y_xgboost_predicted, y_randomforest_predicted, y_test
+    )
     approved = discord_alerter_ask_step(message)
     # mlflow_register_model_step(model=model_XGB, name="model_XGB", metadata=accuracy_xgb)
     # mlflow_register_model_step(model=model_randomforest, name="model_randomforest", metadata=accuracy_randomforest)
